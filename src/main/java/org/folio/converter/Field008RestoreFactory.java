b80a30db97248a27aa65c6334287e453da8a4aa5
Siarhei Charniak <siarhei_charniak@epam.com>
2020-03-24 19:45:00 +0300

    MODQM-2 marc-json <-> ui-compatible-marc-json converter * Added StringConstants containing all string constants * Fixed pom.xml

--- src/main/java/org/folio/converter/Field008RestoreFactory.java
+++ src/main/java/org/folio/converter/Field008RestoreFactory.java
@@ -1,5 +1,53 @@
 package org.folio.converter;
 
+import static org.folio.converter.StringConstants.ACCM;
+import static org.folio.converter.StringConstants.ALPH;
+import static org.folio.converter.StringConstants.AUDN;
+import static org.folio.converter.StringConstants.BIOG;
+import static org.folio.converter.StringConstants.COMP;
+import static org.folio.converter.StringConstants.CONF;
+import static org.folio.converter.StringConstants.CONT;
+import static org.folio.converter.StringConstants.CRTP;
+import static org.folio.converter.StringConstants.CTRY;
+import static org.folio.converter.StringConstants.DATE1;
+import static org.folio.converter.StringConstants.DATE2;
+import static org.folio.converter.StringConstants.DTST;
+import static org.folio.converter.StringConstants.ELEVEN_SPACES;
+import static org.folio.converter.StringConstants.ENTERED;
+import static org.folio.converter.StringConstants.ENTW;
+import static org.folio.converter.StringConstants.FEST;
+import static org.folio.converter.StringConstants.FILE;
+import static org.folio.converter.StringConstants.FIVE_SPACES;
+import static org.folio.converter.StringConstants.FMUS;
+import static org.folio.converter.StringConstants.FORM;
+import static org.folio.converter.StringConstants.FOUR_SPACES;
+import static org.folio.converter.StringConstants.FREQ;
+import static org.folio.converter.StringConstants.GPUB;
+import static org.folio.converter.StringConstants.ILLS;
+import static org.folio.converter.StringConstants.INDX;
+import static org.folio.converter.StringConstants.LANG;
+import static org.folio.converter.StringConstants.LITF;
+import static org.folio.converter.StringConstants.LTXT;
+import static org.folio.converter.StringConstants.MREC;
+import static org.folio.converter.StringConstants.ONE_SPACE;
+import static org.folio.converter.StringConstants.ORIG;
+import static org.folio.converter.StringConstants.PART;
+import static org.folio.converter.StringConstants.PROJ;
+import static org.folio.converter.StringConstants.REGL;
+import static org.folio.converter.StringConstants.RELF;
+import static org.folio.converter.StringConstants.SIX_SPACES;
+import static org.folio.converter.StringConstants.SL;
+import static org.folio.converter.StringConstants.SPFM;
+import static org.folio.converter.StringConstants.SRCE;
+import static org.folio.converter.StringConstants.SRTP;
+import static org.folio.converter.StringConstants.TECH;
+import static org.folio.converter.StringConstants.THREE_SPACES;
+import static org.folio.converter.StringConstants.TIME;
+import static org.folio.converter.StringConstants.TMAT;
+import static org.folio.converter.StringConstants.TRAR;
+import static org.folio.converter.StringConstants.TWO_SPACES;
+import static org.folio.converter.StringConstants.VALUE;
+
 import io.vertx.core.json.JsonObject;
 
 import java.util.EnumMap;
@@ -7,82 +55,75 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Field008RestoreFactory {
-  private static final String ONE_SPACE =     " ";
-  private static final String TWO_SPACES =    "  ";
-  private static final String THREE_SPACES =  "   ";
-  private static final String FOUR_SPACES =   "    ";
-  private static final String FIVE_SPACES =   "     ";
-  private static final String SIX_SPACES =    "      ";
-  private static final String ELEVEN_SPACES = "           ";
 
-  private static EnumMap<ContentType, Field008RestoreStrategy> map = new EnumMap(ContentType.class);
+  private static EnumMap<ContentType, Field008RestoreStrategy> map = new EnumMap<>(ContentType.class);
 
   private Field008RestoreFactory(){}
 
   static {
     map.put(ContentType.BOOKS, jsonObject ->
       restoreHeader(jsonObject)
-        .concat(arrayToString("Ills", jsonObject))
-        .concat(jsonObject.getString("Audn"))
-        .concat(jsonObject.getString("Form"))
-        .concat(arrayToString("Cont", jsonObject))
-        .concat(jsonObject.getString("GPub"))
-        .concat(jsonObject.getString("Conf"))
-        .concat(jsonObject.getString("Fest"))
-        .concat(jsonObject.getString("Indx"))
+        .concat(arrayToString(ILLS, jsonObject))
+        .concat(jsonObject.getString(AUDN))
+        .concat(jsonObject.getString(FORM))
+        .concat(arrayToString(CONT, jsonObject))
+        .concat(jsonObject.getString(GPUB))
+        .concat(jsonObject.getString(CONF))
+        .concat(jsonObject.getString(FEST))
+        .concat(jsonObject.getString(INDX))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("LitF"))
-        .concat(jsonObject.getString("Biog"))
+        .concat(jsonObject.getString(LITF))
+        .concat(jsonObject.getString(BIOG))
         .concat(restoreFooter(jsonObject)));
 
     map.put(ContentType.FILES, jsonObject ->
       restoreHeader(jsonObject)
         .concat(FOUR_SPACES)
-        .concat(jsonObject.getString("Audn"))
-        .concat(jsonObject.getString("Form"))
+        .concat(jsonObject.getString(AUDN))
+        .concat(jsonObject.getString(FORM))
         .concat(TWO_SPACES)
-        .concat(jsonObject.getString("File"))
+        .concat(jsonObject.getString(FILE))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("GPub"))
+        .concat(jsonObject.getString(GPUB))
         .concat(SIX_SPACES)
         .concat(restoreFooter(jsonObject)));
 
     map.put(ContentType.CONTINUING, jsonObject ->
       restoreHeader(jsonObject)
-        .concat(jsonObject.getString("Freq"))
-        .concat(jsonObject.getString("Regl"))
+        .concat(jsonObject.getString(FREQ))
+        .concat(jsonObject.getString(REGL))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("SrTp"))
-        .concat(jsonObject.getString("Orig"))
-        .concat(jsonObject.getString("Form"))
-        .concat(jsonObject.getString("EntW"))
-        .concat(arrayToString("Cont", jsonObject))
-        .concat(jsonObject.getString("GPub"))
-        .concat(jsonObject.getString("Conf"))
+        .concat(jsonObject.getString(SRTP))
+        .concat(jsonObject.getString(ORIG))
+        .concat(jsonObject.getString(FORM))
+        .concat(jsonObject.getString(ENTW))
+        .concat(arrayToString(CONT, jsonObject))
+        .concat(jsonObject.getString(GPUB))
+        .concat(jsonObject.getString(CONF))
         .concat(THREE_SPACES)
-        .concat(jsonObject.getString("Alph"))
-        .concat(jsonObject.getString("S/L"))
+        .concat(jsonObject.getString(ALPH))
+        .concat(jsonObject.getString(SL))
         .concat(restoreFooter(jsonObject)));
 
     map.put(ContentType.MAPS, jsonObject ->
       restoreHeader(jsonObject)
-        .concat(arrayToString("Relf", jsonObject))
-        .concat(jsonObject.getString("Proj"))
+        .concat(arrayToString(RELF, jsonObject))
+        .concat(jsonObject.getString(PROJ))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("CrTp"))
+        .concat(jsonObject.getString(CRTP))
         .concat(TWO_SPACES)
-        .concat(jsonObject.getString("GPub"))
-        .concat(jsonObject.getString("Form"))
+        .concat(jsonObject.getString(GPUB))
+        .concat(jsonObject.getString(FORM))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("Indx"))
+        .concat(jsonObject.getString(INDX))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("SpFm"))
+        .concat(jsonObject.getString(SPFM))
         .concat(restoreFooter(jsonObject)));
 
     map.put(ContentType.MIXED, jsonObject ->
       restoreHeader(jsonObject)
         .concat(FIVE_SPACES)
-        .concat(jsonObject.getString("Form"))
+        .concat(jsonObject.getString(FORM))
         .concat(ELEVEN_SPACES)
         .concat(restoreFooter(jsonObject)));
 
@@ -92,48 +133,48 @@ public class Field008RestoreFactory {
 
     map.put(ContentType.VISUAL, jsonObject ->
       restoreHeader(jsonObject)
-        .concat(arrayToString("Time", jsonObject))
+        .concat(arrayToString(TIME, jsonObject))
         .concat(ONE_SPACE)
-        .concat(jsonObject.getString("Audn"))
+        .concat(jsonObject.getString(AUDN))
         .concat(FIVE_SPACES)
-        .concat(jsonObject.getString("GPub"))
-        .concat(jsonObject.getString("Form"))
+        .concat(jsonObject.getString(GPUB))
+        .concat(jsonObject.getString(FORM))
         .concat(THREE_SPACES)
-        .concat(jsonObject.getString("TMat"))
-        .concat(jsonObject.getString("Tech"))
+        .concat(jsonObject.getString(TMAT))
+        .concat(jsonObject.getString(TECH))
         .concat(restoreFooter(jsonObject)));
 
     map.put(ContentType.UNKNOWN, jsonObject ->
       restoreHeader(jsonObject)
-        .concat(jsonObject.getString("Value"))
+        .concat(jsonObject.getString(VALUE))
         .concat(restoreFooter(jsonObject)));
   }
 
   private static String restoreHeader(JsonObject jsonObject) {
-    return jsonObject.getString("Entered")
-      .concat(jsonObject.getString("DtSt"))
-      .concat(jsonObject.getString("Date1"))
-      .concat(jsonObject.getString("Date2"))
-      .concat(jsonObject.getString("Ctry"));
+    return jsonObject.getString(ENTERED)
+      .concat(jsonObject.getString(DTST))
+      .concat(jsonObject.getString(DATE1))
+      .concat(jsonObject.getString(DATE2))
+      .concat(jsonObject.getString(CTRY));
   }
 
   private static String restoreFooter(JsonObject jsonObject) {
-    return jsonObject.getString("Lang")
-      .concat(jsonObject.getString("MRec"))
-      .concat(jsonObject.getString("Srce"));
+    return jsonObject.getString(LANG)
+      .concat(jsonObject.getString(MREC))
+      .concat(jsonObject.getString(SRCE));
   }
 
   private static String restoreValueForScoresOrSound(JsonObject jsonObject){
     return restoreHeader(jsonObject)
-      .concat(jsonObject.getString("Comp"))
-      .concat(jsonObject.getString("FMus"))
-      .concat(jsonObject.getString("Part"))
-      .concat(jsonObject.getString("Audn"))
-      .concat(jsonObject.getString("Form"))
-      .concat(arrayToString("AccM", jsonObject))
-      .concat(arrayToString("LTxt", jsonObject))
+      .concat(jsonObject.getString(COMP))
+      .concat(jsonObject.getString(FMUS))
+      .concat(jsonObject.getString(PART))
+      .concat(jsonObject.getString(AUDN))
+      .concat(jsonObject.getString(FORM))
+      .concat(arrayToString(ACCM, jsonObject))
+      .concat(arrayToString(LTXT, jsonObject))
       .concat(ONE_SPACE)
-      .concat(jsonObject.getString("TrAr"))
+      .concat(jsonObject.getString(TRAR))
       .concat(ONE_SPACE)
       .concat(restoreFooter(jsonObject));
   }
--- src/main/java/org/folio/converter/Field008SplitterFactory.java
+++ src/main/java/org/folio/converter/Field008SplitterFactory.java
@@ -1,13 +1,52 @@
 package org.folio.converter;
 
+import static org.folio.converter.StringConstants.ACCM;
+import static org.folio.converter.StringConstants.ALPH;
+import static org.folio.converter.StringConstants.AUDN;
+import static org.folio.converter.StringConstants.BIOG;
+import static org.folio.converter.StringConstants.COMP;
+import static org.folio.converter.StringConstants.CONF;
+import static org.folio.converter.StringConstants.CONT;
+import static org.folio.converter.StringConstants.CRTP;
+import static org.folio.converter.StringConstants.CTRY;
+import static org.folio.converter.StringConstants.DATE1;
+import static org.folio.converter.StringConstants.DATE2;
+import static org.folio.converter.StringConstants.DTST;
+import static org.folio.converter.StringConstants.ENTERED;
+import static org.folio.converter.StringConstants.ENTW;
+import static org.folio.converter.StringConstants.FEST;
+import static org.folio.converter.StringConstants.FILE;
+import static org.folio.converter.StringConstants.FMUS;
+import static org.folio.converter.StringConstants.FORM;
+import static org.folio.converter.StringConstants.FREQ;
+import static org.folio.converter.StringConstants.GPUB;
+import static org.folio.converter.StringConstants.ILLS;
+import static org.folio.converter.StringConstants.INDX;
+import static org.folio.converter.StringConstants.LANG;
+import static org.folio.converter.StringConstants.LITF;
+import static org.folio.converter.StringConstants.LTXT;
+import static org.folio.converter.StringConstants.MREC;
+import static org.folio.converter.StringConstants.ORIG;
+import static org.folio.converter.StringConstants.PROJ;
+import static org.folio.converter.StringConstants.REGL;
+import static org.folio.converter.StringConstants.RELF;
+import static org.folio.converter.StringConstants.SL;
+import static org.folio.converter.StringConstants.SPFM;
+import static org.folio.converter.StringConstants.SRCE;
+import static org.folio.converter.StringConstants.SRTP;
+import static org.folio.converter.StringConstants.TECH;
+import static org.folio.converter.StringConstants.TIME;
+import static org.folio.converter.StringConstants.TMAT;
+import static org.folio.converter.StringConstants.TRAR;
+import static org.folio.converter.StringConstants.VALUE;
+
 import io.vertx.core.json.JsonObject;
 
 import java.util.Arrays;
 import java.util.EnumMap;
 
-
 public class Field008SplitterFactory {
-  private static EnumMap<ContentType, Field008SplitStrategy> map = new EnumMap(ContentType.class);
+  private static EnumMap<ContentType, Field008SplitStrategy> map = new EnumMap<>(ContentType.class);
 
   private Field008SplitterFactory(){}
 
@@ -20,54 +59,54 @@ public class Field008SplitterFactory {
         source.substring(20, 21),
         source.substring(21, 22)
       };
-      jsonObject.put("Ills", Arrays.asList(ills));
-      jsonObject.put("Audn", source.substring(22, 23));
-      jsonObject.put("Form", source.substring(23, 24));
+      jsonObject.put(ILLS, Arrays.asList(ills));
+      jsonObject.put(AUDN, source.substring(22, 23));
+      jsonObject.put(FORM, source.substring(23, 24));
       String[] cont = {
         source.substring(24, 25),
         source.substring(25, 26),
         source.substring(26, 27),
         source.substring(27, 28)
       };
-      jsonObject.put("Cont", Arrays.asList(cont));
-      jsonObject.put("GPub", source.substring(28, 29));
-      jsonObject.put("Conf", source.substring(29, 30));
-      jsonObject.put("Fest", source.substring(30, 31));
-      jsonObject.put("Indx", source.substring(31, 32));
-      jsonObject.put("LitF", source.substring(33, 34));
-      jsonObject.put("Biog", source.substring(34, 35));
+      jsonObject.put(CONT, Arrays.asList(cont));
+      jsonObject.put(GPUB, source.substring(28, 29));
+      jsonObject.put(CONF, source.substring(29, 30));
+      jsonObject.put(FEST, source.substring(30, 31));
+      jsonObject.put(INDX, source.substring(31, 32));
+      jsonObject.put(LITF, source.substring(33, 34));
+      jsonObject.put(BIOG, source.substring(34, 35));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
 
     map.put(ContentType.FILES, source -> {
       JsonObject jsonObject = splitHeader(source);
-      jsonObject.put("Audn", source.substring(22, 23));
-      jsonObject.put("Form", source.substring(23, 24));
-      jsonObject.put("File", source.substring(26, 27));
-      jsonObject.put("GPub", source.substring(28, 29));
+      jsonObject.put(AUDN, source.substring(22, 23));
+      jsonObject.put(FORM, source.substring(23, 24));
+      jsonObject.put(FILE, source.substring(26, 27));
+      jsonObject.put(GPUB, source.substring(28, 29));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
 
     map.put(ContentType.CONTINUING, source -> {
       JsonObject jsonObject = splitHeader(source);
-      jsonObject.put("Freq", source.substring(18, 19));
-      jsonObject.put("Regl", source.substring(19, 20));
-      jsonObject.put("SrTp", source.substring(21, 22));
-      jsonObject.put("Orig", source.substring(22, 23));
-      jsonObject.put("Form", source.substring(23, 24));
-      jsonObject.put("EntW", source.substring(24, 25));
+      jsonObject.put(FREQ, source.substring(18, 19));
+      jsonObject.put(REGL, source.substring(19, 20));
+      jsonObject.put(SRTP, source.substring(21, 22));
+      jsonObject.put(ORIG, source.substring(22, 23));
+      jsonObject.put(FORM, source.substring(23, 24));
+      jsonObject.put(ENTW, source.substring(24, 25));
       String[] cont = {
         source.substring(25, 26),
         source.substring(26, 27),
         source.substring(27, 28),
       };
-      jsonObject.put("Cont", Arrays.asList(cont));
-      jsonObject.put("GPub", source.substring(28, 29));
-      jsonObject.put("Conf", source.substring(29, 30));
-      jsonObject.put("Alph", source.substring(33, 34));
-      jsonObject.put("S/L", source.substring(34, 35));
+      jsonObject.put(CONT, Arrays.asList(cont));
+      jsonObject.put(GPUB, source.substring(28, 29));
+      jsonObject.put(CONF, source.substring(29, 30));
+      jsonObject.put(ALPH, source.substring(33, 34));
+      jsonObject.put(SL, source.substring(34, 35));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
@@ -80,20 +119,20 @@ public class Field008SplitterFactory {
         source.substring(20, 21),
         source.substring(21, 22)
       };
-      jsonObject.put("Relf", Arrays.asList(relf));
-      jsonObject.put("Proj", source.substring(22, 24));
-      jsonObject.put("CrTp", source.substring(25, 26));
-      jsonObject.put("GPub", source.substring(28, 29));
-      jsonObject.put("Form", source.substring(29, 30));
-      jsonObject.put("Indx", source.substring(31, 32));
-      jsonObject.put("SpFm", source.substring(33, 35));
+      jsonObject.put(RELF, Arrays.asList(relf));
+      jsonObject.put(PROJ, source.substring(22, 24));
+      jsonObject.put(CRTP, source.substring(25, 26));
+      jsonObject.put(GPUB, source.substring(28, 29));
+      jsonObject.put(FORM, source.substring(29, 30));
+      jsonObject.put(INDX, source.substring(31, 32));
+      jsonObject.put(SPFM, source.substring(33, 35));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
 
     map.put(ContentType.MIXED, source -> {
       JsonObject jsonObject = splitHeader(source);
-      jsonObject.put("Form", source.substring(23, 24));
+      jsonObject.put(FORM, source.substring(23, 24));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
@@ -109,19 +148,19 @@ public class Field008SplitterFactory {
         source.substring(19, 20),
         source.substring(20, 21)
       };
-      jsonObject.put("Time", Arrays.asList(time));
-      jsonObject.put("Audn", source.substring(22, 23));
-      jsonObject.put("GPub", source.substring(28, 29));
-      jsonObject.put("Form", source.substring(29, 30));
-      jsonObject.put("TMat", source.substring(33, 34));
-      jsonObject.put("Tech", source.substring(34, 35));
+      jsonObject.put(TIME, Arrays.asList(time));
+      jsonObject.put(AUDN, source.substring(22, 23));
+      jsonObject.put(GPUB, source.substring(28, 29));
+      jsonObject.put(FORM, source.substring(29, 30));
+      jsonObject.put(TMAT, source.substring(33, 34));
+      jsonObject.put(TECH, source.substring(34, 35));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
 
     map.put(ContentType.UNKNOWN, source -> {
       JsonObject jsonObject = splitHeader(source);
-      jsonObject.put("Value", source.substring(18, 35));
+      jsonObject.put(VALUE, source.substring(18, 35));
       jsonObject.mergeIn(splitFooter(source));
       return jsonObject;
     });
@@ -129,29 +168,29 @@ public class Field008SplitterFactory {
 
   private static JsonObject splitHeader(String source) {
     JsonObject jsonObject = new JsonObject();
-    jsonObject.put("Entered", source.substring(0, 6));
-    jsonObject.put("DtSt", source.substring(6, 7));
-    jsonObject.put("Date1", source.substring(7, 11));
-    jsonObject.put("Date2", source.substring(11, 15));
-    jsonObject.put("Ctry", source.substring(15, 18));
+    jsonObject.put(ENTERED, source.substring(0, 6));
+    jsonObject.put(DTST, source.substring(6, 7));
+    jsonObject.put(DATE1, source.substring(7, 11));
+    jsonObject.put(DATE2, source.substring(11, 15));
+    jsonObject.put(CTRY, source.substring(15, 18));
     return jsonObject;
   }
 
   private static JsonObject splitFooter(String source) {
     JsonObject jsonObject = new JsonObject();
-    jsonObject.put("Lang", source.substring(35, 38));
-    jsonObject.put("MRec", source.substring(38, 39));
-    jsonObject.put("Srce", source.substring(39, 40));
+    jsonObject.put(LANG, source.substring(35, 38));
+    jsonObject.put(MREC, source.substring(38, 39));
+    jsonObject.put(SRCE, source.substring(39, 40));
     return jsonObject;
   }
 
   private static JsonObject splitField008ForScoresOrSound(String source) {
     JsonObject jsonObject = splitHeader(source);
-    jsonObject.put("Comp", source.substring(18, 20));
-    jsonObject.put("FMus", source.substring(20, 21));
-    jsonObject.put("Part", source.substring(21, 22));
-    jsonObject.put("Audn", source.substring(22, 23));
-    jsonObject.put("Form", source.substring(23, 24));
+    jsonObject.put(COMP, source.substring(18, 20));
+    jsonObject.put(FMUS, source.substring(20, 21));
+    jsonObject.put(FMUS, source.substring(21, 22));
+    jsonObject.put(AUDN, source.substring(22, 23));
+    jsonObject.put(FORM, source.substring(23, 24));
     String[] accm = {
       source.substring(24, 25),
       source.substring(25, 26),
@@ -160,13 +199,13 @@ public class Field008SplitterFactory {
       source.substring(28, 29),
       source.substring(29, 30)
     };
-    jsonObject.put("AccM", Arrays.asList(accm));
+    jsonObject.put(ACCM, Arrays.asList(accm));
     String[] ltxt = {
       source.substring(30, 31),
       source.substring(31, 32)
     };
-    jsonObject.put("LTxt", Arrays.asList(ltxt));
-    jsonObject.put("TrAr", source.substring(33, 34));
+    jsonObject.put(LTXT, Arrays.asList(ltxt));
+    jsonObject.put(TRAR, source.substring(33, 34));
     jsonObject.mergeIn(splitFooter(source));
     return jsonObject;
   }

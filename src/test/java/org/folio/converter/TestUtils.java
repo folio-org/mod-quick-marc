aa5c799421de984f52de6a176b777e215437c9b2
khandramai <Viachaslau_Khandramai@epam.com>
2020-05-21 21:05:34 +0300

    MODQM-16 # Refactoring/improvements

--- src/test/java/org/folio/converter/TestUtils.java
+++ src/test/java/org/folio/converter/TestUtils.java
@@ -5,13 +5,19 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
+import org.folio.rest.jaxrs.model.Field;
+import org.folio.rest.jaxrs.model.QuickMarcJson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.json.JsonObject;
+
+import static org.folio.converter.QuickMarcToParsedRecordConverterTest.TESTED_TAG_NAME;
 
 public class TestUtils {
   private static final Logger logger = LoggerFactory.getLogger(TestUtils.class);
@@ -39,5 +45,18 @@ public class TestUtils {
         return sb.toString();
       }
     }
+  }
+
+  public static Field getFieldWithIndicators(List<Object> indicators) {
+    return new Field()
+      .withTag(TESTED_TAG_NAME)
+      .withContent("$333 content")
+      .withIndicators(indicators);
+  }
+
+  public static QuickMarcJson getQuickMarcJsonWithMinContent(Field... fields) {
+    return new QuickMarcJson()
+      .withLeader("01542ccm a2200361   4500")
+      .withFields(Arrays.asList(fields));
   }
 }

ba6cfffe97409ba10d44b5f03de23eca59c9ab72
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2021-03-24 12:39:37 +0200

    MODQM-84 Add standard admin healthcheck endpoint (#77)

--- run-env.sh
+++ run-env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DB_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
-KAFKA_URL="${KAFKA_HOST}:${KAFKA_PORT}"
-OPTS="-Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD} -Dspring.datasource.url=${DB_URL} -Dspring.kafka.bootstrap-servers=${KAFKA_URL}"
+DB_URL="jdbc:postgresql://${DB_HOST:-localhost}:${DB_PORT:-5432}/${DB_DATABASE:-db}"
+KAFKA_URL="${KAFKA_HOST:-localhost}:${KAFKA_PORT:-9092}"
+OPTS="-Dspring.datasource.username=${DB_USERNAME:-user} -Dspring.datasource.password=${DB_PASSWORD:-pass} -Dspring.datasource.url=${DB_URL} -Dspring.kafka.bootstrap-servers=${KAFKA_URL}"
 export JAVA_OPTIONS="${JAVA_OPTIONS:-} ${OPTS}"

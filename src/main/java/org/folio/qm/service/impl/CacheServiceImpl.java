cf427963601fd71a2d0a244734793085d4db3291
Pavlo Smahin <Pavlo_Smahin@epam.com>
2021-07-26 14:33:43 +0300

    MODQM-138 Make CacheService configurable

--- src/main/java/org/folio/qm/service/impl/CacheServiceImpl.java
+++ src/main/java/org/folio/qm/service/impl/CacheServiceImpl.java
@@ -1,8 +1,11 @@
 package org.folio.qm.service.impl;
 
+import java.util.concurrent.TimeUnit;
+
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.async.DeferredResult;
 
@@ -13,7 +16,15 @@ import org.folio.qm.service.CacheService;
 @SuppressWarnings("rawtypes")
 public class CacheServiceImpl implements CacheService<DeferredResult> {
 
-  private final Cache<String, DeferredResult> cache = CacheBuilder.newBuilder().maximumSize(1000).build();
+  private final Cache<String, DeferredResult> cache;
+
+  public CacheServiceImpl(@Value("${folio.qm.cache.update.max-size:1000}") int maximumSize,
+                          @Value("${folio.qm.cache.update.duration.min:10}") int duration) {
+    this.cache = CacheBuilder.newBuilder()
+      .maximumSize(maximumSize)
+      .expireAfterWrite(duration, TimeUnit.MINUTES)
+      .build();
+  }
 
   @Override
   public void putToCache(String key, DeferredResult object) {

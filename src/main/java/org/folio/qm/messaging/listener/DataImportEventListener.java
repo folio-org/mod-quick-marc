4b2f25c53a183f0ce2d77a3746459bc89f7d2552
Pavlo Smahin <Pavlo_Smahin@epam.com>
2022-02-09 21:20:28 +0200

    MODQM-197 Fix kafka configuration

--- src/main/java/org/folio/qm/messaging/listener/DataImportEventListener.java
+++ src/main/java/org/folio/qm/messaging/listener/DataImportEventListener.java
@@ -14,17 +14,26 @@ import org.folio.rest.jaxrs.model.DataImportEventPayload;
 @RequiredArgsConstructor
 public class DataImportEventListener {
 
+  public static final String DI_COMPLETED_LISTENER_ID = "quick-marc-di-completed-listener";
+  public static final String DI_ERROR_LISTENER_ID = "quick-marc-di-error-listener";
+
   private final DataImportEventProcessingService processingService;
 
-  @KafkaListener(groupId = "#{@groupIdProvider.diCompletedGroupId()}",
-    topicPattern = "#{@topicPatternProvider.diCompletedTopicName()}",
+  @KafkaListener(
+    id = DI_COMPLETED_LISTENER_ID,
+    groupId = "#{folioKafkaProperties.listener['di-completed'].groupId}",
+    topicPattern = "#{folioKafkaProperties.listener['di-completed'].topicPattern}",
+    concurrency = "#{folioKafkaProperties.listener['di-completed'].concurrency}",
     containerFactory = "dataImportKafkaListenerContainerFactory")
   public void diCompletedListener(DataImportEventPayload data, MessageHeaders messageHeaders) {
     processingService.processDICompleted(data);
   }
 
-  @KafkaListener(groupId = "#{@groupIdProvider.diErrorGroupId()}",
-    topicPattern = "#{@topicPatternProvider.diErrorTopicName()}",
+  @KafkaListener(
+    id = DI_ERROR_LISTENER_ID,
+    groupId = "#{folioKafkaProperties.listener['di-error'].groupId}",
+    topicPattern = "#{folioKafkaProperties.listener['di-error'].topicPattern}",
+    concurrency = "#{folioKafkaProperties.listener['di-error'].concurrency}",
     containerFactory = "dataImportKafkaListenerContainerFactory")
   public void diErrorListener(DataImportEventPayload data, MessageHeaders messageHeaders) {
     processingService.processDIError(data);

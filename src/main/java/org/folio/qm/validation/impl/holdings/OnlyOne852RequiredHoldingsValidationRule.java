4a2f35c5515e4ac8561f9f62d4c44c607f4f49f8
Pavel Smagin <Pavlo_Smahin@epam.com>
2022-03-10 11:41:57 +0200

    MODQM-204 Change validation for holdings 852 tag (#149)
    * MODQM-204 Change validation for holdings 852 tag
    
    * MODQM-204 Add validation to record creation
    
    * MODQM-204 Fix test
    
    (cherry picked from commit c5f02f3fe2668a9607a5018e5461f60806c1f2e2)

--- src/main/java/org/folio/qm/validation/impl/holdings/OnlyOne852RequiredHoldingsValidationRule.java
+++ src/main/java/org/folio/qm/validation/impl/holdings/OnlyOne852RequiredHoldingsValidationRule.java
@@ -9,14 +9,14 @@ import org.folio.qm.domain.dto.FieldItem;
 import org.folio.qm.validation.ValidationError;
 
 @Component
-public class AtLeastOne852RequiredHoldingsValidationRule extends AbstractHoldingsValidationRule {
+public class OnlyOne852RequiredHoldingsValidationRule extends AbstractHoldingsValidationRule {
 
   private static final String TAG_CODE = "852";
 
   @Override
   public Optional<ValidationError> validate(List<FieldItem> fieldItems) {
     List<FieldItem> fieldsWith852TagCode = filterFieldsByTagCode(fieldItems, TAG_CODE);
-    return atLeastOneRequiredCondition().apply(TAG_CODE, fieldsWith852TagCode);
+    return onlyOneRequiredCondition().apply(TAG_CODE, fieldsWith852TagCode);
   }
 
 }

c4b8a25910249940859903648f983c96b08a0ee5
psmagin <Pavlo_Smahin@epam.com>
2022-04-04 17:05:34 +0300

    MODQM-218 Fix test

--- src/main/java/org/folio/qm/controller/RecordsEditorAsyncApiImpl.java
+++ src/main/java/org/folio/qm/controller/RecordsEditorAsyncApiImpl.java
@@ -3,7 +3,6 @@ package org.folio.qm.controller;
 import java.util.UUID;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

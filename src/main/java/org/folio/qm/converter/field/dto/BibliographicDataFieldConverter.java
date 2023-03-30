ac651a416d0cdd24843230e68fd6d82c78eebab8
hasan <khasan_sidikov@epam.com>
2023-03-30 12:38:32 +0500

    MODQM-319: Removed unneeded bean from BibliographicDataFieldConverter

--- src/main/java/org/folio/qm/converter/field/dto/BibliographicDataFieldConverter.java
+++ src/main/java/org/folio/qm/converter/field/dto/BibliographicDataFieldConverter.java
@@ -3,8 +3,6 @@ package org.folio.qm.converter.field.dto;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import lombok.RequiredArgsConstructor;
-import org.folio.qm.client.LinksClient;
 import org.folio.qm.domain.dto.FieldItem;
 import org.folio.qm.domain.dto.MarcFormat;
 import org.folio.qm.util.MarcUtils;
@@ -15,11 +13,9 @@ import org.marc4j.marc.VariableField;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class BibliographicDataFieldConverter extends CommonDataFieldConverter {
   private static final char AUTHORITY_ID_SUBFIELD_CODE = '9';
   private static final char AUTHORITY_NATURAL_ID_SUBFIELD_CODE = '0';
-  private final LinksClient linksClient;
 
   @Override
   public FieldItem convert(DataField field, Leader leader) {

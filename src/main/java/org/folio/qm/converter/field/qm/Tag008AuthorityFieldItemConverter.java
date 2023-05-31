a350ed6ca3899732e32d5e54bc3d3dbc9afbbac8
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2023-05-31 13:55:17 +0200

    fix(field-system-population): Set 'entered' date when absent/invalid on 008 field for authorities
    - Add 'Entered' date population/correction to `Tag008AuthorityFieldItemConverter`
    
    Closes: MODQM-346

--- src/main/java/org/folio/qm/converter/field/qm/Tag008AuthorityFieldItemConverter.java
+++ src/main/java/org/folio/qm/converter/field/qm/Tag008AuthorityFieldItemConverter.java
@@ -3,11 +3,11 @@ package org.folio.qm.converter.field.qm;
 import static org.folio.qm.converter.elements.Constants.AUTHORITY_CONTROL_FIELD_ITEMS;
 import static org.folio.qm.converter.elements.Constants.TAG_008_AUTHORITY_CONTROL_FIELD_LENGTH;
 import static org.folio.qm.converter.elements.Constants.TAG_008_CONTROL_FIELD;
+import static org.folio.qm.converter.elements.ControlFieldItem.DATE_ENTERED;
 import static org.folio.qm.util.MarcUtils.restoreBlanks;
 
 import jakarta.validation.constraints.NotNull;
 import java.util.Map;
-import org.folio.qm.converter.field.FieldItemConverter;
 import org.folio.qm.domain.dto.FieldItem;
 import org.folio.qm.domain.dto.MarcFormat;
 import org.marc4j.marc.VariableField;
@@ -15,7 +15,7 @@ import org.marc4j.marc.impl.ControlFieldImpl;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Tag008AuthorityFieldItemConverter implements FieldItemConverter {
+public class Tag008AuthorityFieldItemConverter extends Tag008FieldItemConverter {
 
   @Override
   public VariableField convert(FieldItem field) {
@@ -30,6 +30,8 @@ public class Tag008AuthorityFieldItemConverter implements FieldItemConverter {
   private String restoreControlField(@NotNull Object content) {
     @SuppressWarnings("unchecked")
     var contentMap = (Map<String, Object>) content;
+    setDateEntered(DATE_ENTERED.getName(), contentMap);
+
     return restoreFixedLengthField(contentMap, TAG_008_AUTHORITY_CONTROL_FIELD_LENGTH, 0,
       AUTHORITY_CONTROL_FIELD_ITEMS);
   }

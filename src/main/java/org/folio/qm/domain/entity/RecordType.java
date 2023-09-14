722482ae84717e00b92b0f7d26c8fc7ca8978c69
pawelmol <pawel@mol.com.pl>
2023-09-14 13:29:32 +0200

    Changed API path parameter percordType to lower case.

--- src/main/java/org/folio/qm/domain/entity/RecordType.java
+++ src/main/java/org/folio/qm/domain/entity/RecordType.java
@@ -1,9 +1,36 @@
 package org.folio.qm.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RecordType {
 
-  MARC_BIBLIOGRAPHIC,
-  MARC_HOLDINGS,
-  MARC_AUTHORITY
+  MARC_BIBLIOGRAPHIC("bibliographic"),
+  MARC_HOLDINGS("holdings"),
+  MARC_AUTHORITY("authority");
+  private String value;
 
+  RecordType(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static RecordType fromValue(String value) {
+    for (RecordType b : RecordType.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
 }

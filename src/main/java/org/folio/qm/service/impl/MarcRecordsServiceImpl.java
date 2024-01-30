cc7c213f270f8ad8310dd88832fe534b6376b568
Tsaghik Khachatryan <khachatryan.tsaghik@gmail.com>
2024-01-30 19:06:21 +0400

    refactor(00X fields positions):(Allow for any MARC field 00X to be moved up and down):
    dummy logs
    
    Closes: MOSQM-379

--- src/main/java/org/folio/qm/service/impl/MarcRecordsServiceImpl.java
+++ src/main/java/org/folio/qm/service/impl/MarcRecordsServiceImpl.java
@@ -110,6 +110,7 @@ public class MarcRecordsServiceImpl implements MarcRecordsService {
     System.out.println("tsaghik parsedRecordDto: " + parsedRecordDto);
     updateResult.onCompletion(updateLinksTask(folioExecutionContext, quickMarc, updateResult));
     changeManagerService.putParsedRecordByInstanceId(quickMarc.getParsedRecordDtoId(), parsedRecordDto);
+    System.out.println("tsaghik updateResult: " + updateResult);
     log.info("updateById:: quickMarc updated by parsedRecordId: {}", parsedRecordId);
   }
 

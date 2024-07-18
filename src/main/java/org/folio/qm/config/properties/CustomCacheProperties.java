a9221a6decede91d39a04bf4f8abbc45449e367f
psmagin <pavlo_smahin@epam.com>
2024-07-18 14:53:12 +0300

    - increase specifications cache ttl

---
+++ src/main/java/org/folio/qm/config/properties/CustomCacheProperties.java
@@ -0,0 +1,17 @@
+package org.folio.qm.config.properties;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "folio.cache")
+public class CustomCacheProperties {
+
+  private final Map<String, CustomCache> spec = new HashMap<>();
+
+  public record CustomCache(long maximumSize, Duration ttl) { }
+
+}

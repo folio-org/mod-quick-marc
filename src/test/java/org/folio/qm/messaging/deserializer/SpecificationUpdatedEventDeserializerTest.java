c27918472ef040e2ace79de7689d70727261f6ce
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2024-07-30 17:37:04 +0200

    Improve test coverage

--- src/test/java/org/folio/qm/messaging/deserializer/SpecificationUpdatedEventDeserializerTest.java
+++ src/test/java/org/folio/qm/messaging/deserializer/SpecificationUpdatedEventDeserializerTest.java
@@ -1,5 +1,6 @@
 package org.folio.qm.messaging.deserializer;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.qm.support.utils.ApiTestUtils.TENANT_ID;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -20,6 +21,17 @@ class SpecificationUpdatedEventDeserializerTest {
   private final ObjectMapper objectMapper = spy(ObjectMapper.class);
   private final SpecificationUpdatedEventDeserializer deserializer =
     new SpecificationUpdatedEventDeserializer(objectMapper);
+
+  @Test
+  @SneakyThrows
+  void deserialize() {
+    var expected = new SpecificationUpdatedEvent(UUID.randomUUID(), TENANT_ID);
+    var serialized = objectMapper.writeValueAsBytes(expected);
+
+    var actual = deserializer.deserialize("test", serialized);
+
+    assertThat(actual).isEqualTo(expected);
+  }
 
   @Test
   @SneakyThrows

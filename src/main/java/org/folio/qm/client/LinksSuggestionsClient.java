bb75862d838549d3260dac62250bbdb013314abf
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2026-02-11 15:11:39 +0200

    fix(links-suggestions): fix linksSuggestionsClient (#362)

--- src/main/java/org/folio/qm/client/LinksSuggestionsClient.java
+++ src/main/java/org/folio/qm/client/LinksSuggestionsClient.java
@@ -3,14 +3,16 @@ package org.folio.qm.client;
 import org.folio.qm.client.model.EntitiesLinksSuggestions;
 import org.folio.qm.domain.dto.AuthoritySearchParameter;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange(url = "links-suggestions", accept = MediaType.APPLICATION_JSON_VALUE)
 public interface LinksSuggestionsClient {
 
-  @HttpExchange("/marc")
-  EntitiesLinksSuggestions postLinksSuggestions(EntitiesLinksSuggestions srsMarcRecord,
+  @PostExchange("/marc")
+  EntitiesLinksSuggestions postLinksSuggestions(@RequestBody EntitiesLinksSuggestions srsMarcRecord,
                                                 @RequestParam AuthoritySearchParameter authoritySearchParameter,
                                                 @RequestParam Boolean ignoreAutoLinkingEnabled);
 }

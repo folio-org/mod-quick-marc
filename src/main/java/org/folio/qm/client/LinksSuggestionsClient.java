24415a17f4d29075a8d8093fb1d758991a70935b
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2026-02-11 14:20:51 +0200

    fix(links-suggestions): add RequestBody annotation to the postLinksSuggestions

--- src/main/java/org/folio/qm/client/LinksSuggestionsClient.java
+++ src/main/java/org/folio/qm/client/LinksSuggestionsClient.java
@@ -3,6 +3,7 @@ package org.folio.qm.client;
 import org.folio.qm.client.model.EntitiesLinksSuggestions;
 import org.folio.qm.domain.dto.AuthoritySearchParameter;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.HttpExchange;
 
@@ -10,7 +11,7 @@ import org.springframework.web.service.annotation.HttpExchange;
 public interface LinksSuggestionsClient {
 
   @HttpExchange("/marc")
-  EntitiesLinksSuggestions postLinksSuggestions(EntitiesLinksSuggestions srsMarcRecord,
+  EntitiesLinksSuggestions postLinksSuggestions(@RequestBody EntitiesLinksSuggestions srsMarcRecord,
                                                 @RequestParam AuthoritySearchParameter authoritySearchParameter,
                                                 @RequestParam Boolean ignoreAutoLinkingEnabled);
 }
